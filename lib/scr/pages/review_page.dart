50e5cccfee5d5959218dfbbeda5594231a65f963
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-08 08:29:13 -0500

    Create review_page.dart

---
+++ lib/scr/pages/review_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ReviewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 99.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          height: 10.0,
+          color: Colors.red,
+        ),
+      ],
+    );
+  }
+}
