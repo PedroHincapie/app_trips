462f240934d269a4e89a541f6d6162106ebc97d0
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-08 10:04:08 -0500

    Update review_page.dart

--- lib/scr/pages/review_page.dart
+++ lib/scr/pages/review_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ReviewPage extends StatelessWidget {
@@ -11,20 +12,27 @@ class ReviewPage extends StatelessWidget {
   }
 
   Container _crearFoto() {
+    final containerFoto = Container(
+      child: FadeInImage(
+        placeholder: AssetImage('assets/images/jar-loading.gif'),
+        image: NetworkImage(
+            'https://vignette.wikia.nocookie.net/disney/images/5/53/Stan_Lee.jpg/revision/latest/top-crop/width/360/height/450?cb=20181204153103'),
+        fadeInDuration: Duration(milliseconds: 200),
+        fit: BoxFit.cover,
+        width: 80.0,
+        height: 80.0,
+      ),
+    );
+
     return Container(
       margin: EdgeInsets.only(
-        top: 90.0,
+        top: 60.0,
         left: 20.0,
         right: 20.0,
       ),
-      height: 80.0,
-      width: 80.0,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.red,
-        image: DecorationImage(
-          image: AssetImage('assets/images/Stan_Lee.jpg'),
-        ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(35.0),
+        child: containerFoto,
       ),
     );
   }
