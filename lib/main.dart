154b8985a4bf95f07d60a5ee79ecf0f8fa1cfce6
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-07 22:44:42 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
+import 'package:app_trips/pages/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
-  }
-}
+
