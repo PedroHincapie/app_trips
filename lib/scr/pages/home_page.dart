19c51e9cb4ee308ec54aafb27ed4d3bc3c62c9ef
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-08 10:52:12 -0500

    Update home_page.dart

--- lib/scr/pages/home_page.dart
+++ lib/scr/pages/home_page.dart
@@ -8,6 +8,13 @@ class HomePage extends StatelessWidget {
       """Lorem Ipsum is simply dummy text of the printing and typesetting industry.
      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley """;
 
+  String urlImagen =
+      'https://vignette.wikia.nocookie.net/disney/images/5/53/Stan_Lee.jpg/revision/latest/top-crop/width/360/height/450?cb=20181204153103';
+
+  String nombreUsuario = 'Varuna Yasas';
+  String detalles = '1 review 5 photos';
+  String comentario = 'There is an amazing place Sri lanka';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +28,12 @@ class HomePage extends StatelessWidget {
             cantidadEstrellas: 4.6,
             descripcionSitio: _texto,
           ),
-          ReviewPage()
+          ReviewPage(
+            comentario: comentario,
+            detalles: detalles,
+            nombreUsuario: nombreUsuario,
+            urlImagen: urlImagen,
+          )
         ],
       ),
     );
