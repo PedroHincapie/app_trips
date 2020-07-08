13bae8a431416c49bcc7aac3a3fd1b5b618c2907
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-08 09:09:56 -0500

    Update review_page.dart

--- lib/scr/pages/review_page.dart
+++ lib/scr/pages/review_page.dart
@@ -5,16 +5,27 @@ class ReviewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(
-            top: 99.0,
-            left: 20.0,
-            right: 20.0,
-          ),
-          height: 10.0,
-          color: Colors.red,
-        ),
+        _crearFoto(),
       ],
+    );
+  }
+
+  Container _crearFoto() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 90.0,
+        left: 20.0,
+        right: 20.0,
+      ),
+      height: 80.0,
+      width: 80.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.red,
+        image: DecorationImage(
+          image: AssetImage('assets/images/Stan_Lee.jpg'),
+        ),
+      ),
     );
   }
 }
