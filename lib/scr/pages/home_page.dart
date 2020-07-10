18929c7f532ceeb4374424d5e5893e280810f096
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-10 11:18:12 -0500

    Update home_page.dart

--- lib/scr/pages/home_page.dart
+++ lib/scr/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:app_trips/scr/pages/review_page.dart';
+import 'package:app_trips/scr/pages/review_list_page.dart';
 import 'package:flutter/material.dart';
 
 import 'description_place_page.dart';
@@ -7,14 +7,6 @@ class HomePage extends StatelessWidget {
   final String _texto =
       """Lorem Ipsum is simply dummy text of the printing and typesetting industry.
      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley """;
-
-  String urlImagen =
-      'https://vignette.wikia.nocookie.net/disney/images/5/53/Stan_Lee.jpg/revision/latest/top-crop/width/360/height/450?cb=20181204153103';
-
-  String nombreUsuario = 'Varuna Yasas';
-  String detalles = '1 review 5 photos';
-  String comentario = 'There is an amazing place Sri lanka';
-  double cantidadEstrellas = 2.10;
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +21,7 @@ class HomePage extends StatelessWidget {
             cantidadEstrellas: 3.8,
             descripcionSitio: _texto,
           ),
-          ReviewPage(
-            comentario: comentario,
-            detalles: detalles,
-            nombreUsuario: nombreUsuario,
-            urlImagen: urlImagen,
-            cantidadEstrellas: cantidadEstrellas,
-          )
+          ReviewListPage()
         ],
       ),
     );
