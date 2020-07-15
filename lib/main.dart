bd26937e4f812a629d45f60376ae61b9b54a56e2
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-14 23:11:57 -0500

    Borrar el StatusBar de Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:app_trips/scr/pages/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarBrightness: Brightness.light,
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(MyApp());
+}
