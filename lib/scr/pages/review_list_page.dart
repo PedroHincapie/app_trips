189065cfcc61ca8572fc2422665e8384871f4f7f
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-14 22:58:14 -0500

    Update review_list_page.dart

--- lib/scr/pages/review_list_page.dart
+++ lib/scr/pages/review_list_page.dart
@@ -2,8 +2,13 @@ import 'package:app_trips/scr/pages/review_page.dart';
 import 'package:flutter/material.dart';
 
 class ReviewListPage extends StatelessWidget {
-  String urlImagen =
+  final urlImagen =
       'https://vignette.wikia.nocookie.net/disney/images/5/53/Stan_Lee.jpg/revision/latest/top-crop/width/360/height/450?cb=20181204153103';
+
+  final urlImagen2 =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT-u1BHaJzPFxfhmNuA1Qo6jXpCfJITKNeOxw&usqp=CAU';
+  final urlImagen3 =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYU69duIIrQrD_S0Z6J2dJZXjfJXAiy_cKog&usqp=CAU';
 
   String nombreUsuario = 'Varuna Yasas';
   String detalles = '1 review 5 photos';
@@ -22,7 +27,13 @@ class ReviewListPage extends StatelessWidget {
             comentario: comentario,
             cantidadEstrellas: cantidadEstrellas),
         ReviewPage(
-            urlImagen: urlImagen,
+            urlImagen: urlImagen2,
+            nombreUsuario: nombreUsuario,
+            detalles: detalles,
+            comentario: comentario,
+            cantidadEstrellas: cantidadEstrellas),
+        ReviewPage(
+            urlImagen: urlImagen3,
             nombreUsuario: nombreUsuario,
             detalles: detalles,
             comentario: comentario,
@@ -34,7 +45,13 @@ class ReviewListPage extends StatelessWidget {
             comentario: comentario,
             cantidadEstrellas: cantidadEstrellas),
         ReviewPage(
-            urlImagen: urlImagen,
+            urlImagen: urlImagen2,
+            nombreUsuario: nombreUsuario,
+            detalles: detalles,
+            comentario: comentario,
+            cantidadEstrellas: cantidadEstrellas),
+        ReviewPage(
+            urlImagen: urlImagen3,
             nombreUsuario: nombreUsuario,
             detalles: detalles,
             comentario: comentario,
