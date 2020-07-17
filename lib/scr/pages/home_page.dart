99a9868661168dbe57db3d83907f86c42cbfd599
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-16 22:14:07 -0500

    Update home_page.dart

--- lib/scr/pages/home_page.dart
+++ lib/scr/pages/home_page.dart
@@ -6,9 +6,7 @@ import 'description_place_page.dart';
 
 class HomePage extends StatelessWidget {
   final String _texto =
-      """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
-      of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,remaining essentially unchanged.
-      It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum""";
+      '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,remaining essentially unchanged.''';
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +17,7 @@ class HomePage extends StatelessWidget {
             children: <Widget>[
               DescriptionPlacePage(
                 nombreSitio: 'Duwili Ella',
-                cantidadEstrellas: 3.8,
+                cantidadEstrellas: 5.0,
                 descripcionSitio: _texto,
               ),
               ReviewListPage(),
