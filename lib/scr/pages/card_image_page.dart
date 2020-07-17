762f09e950dcc5c79d8f7ea89df7ad61747fe182
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-16 21:54:48 -0500

    Create card_image_page.dart

---
+++ lib/scr/pages/card_image_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CardImagePage extends StatelessWidget {
+  String _urlImagen = 'assets/images/cap.jpg';
+
+  CardImagePage(this._urlImagen);
+
+  @override
+  Widget build(BuildContext context) {
+    final card = Container(
+      height: 350.0,
+      width: 250.0,
+      margin: EdgeInsets.only(
+        top: 80.0,
+        left: 20.0,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        shape: BoxShape.rectangle,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 15.0,
+            offset: Offset(0.0, 7.0),
+          ),
+        ],
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(_urlImagen),
+        ),
+      ),
+    );
+
+    return card;
+  }
+}
