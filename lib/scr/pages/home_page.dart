6bf77a486349f2db825f41fa395c313ed77eccd1
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-16 21:54:59 -0500

    Update home_page.dart

--- lib/scr/pages/home_page.dart
+++ lib/scr/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:app_trips/scr/pages/gradiente_back_page.dart';
+import 'package:app_trips/scr/pages/header_app_bar_page.dart';
 import 'package:app_trips/scr/pages/review_list_page.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +6,9 @@ import 'description_place_page.dart';
 
 class HomePage extends StatelessWidget {
   final String _texto =
-      """Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley """;
+      """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
+      of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,remaining essentially unchanged.
+      It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum""";
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +25,7 @@ class HomePage extends StatelessWidget {
               ReviewListPage(),
             ],
           ),
-          GradieBackPage(),
+          HeaderAppBarPage()
         ],
       ),
     );
