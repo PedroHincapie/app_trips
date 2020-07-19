c737f91dcc805522dfffab0d6779781c13b7a7b0
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-19 15:11:26 -0500

    Create button_purple_page.dart

---
+++ lib/scr/pages/button_purple_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ButtonPurplePage extends StatelessWidget {
+  String _buttonText = 'Navigate';
+
+  ButtonPurplePage(this._buttonText);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 30,
+          left: 20.0,
+          right: 20.0,
+        ),
+        height: 50.0,
+        width: 180.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF4248D3),
+              Color(0xFF584CD1),
+            ],
+            begin: FractionalOffset(0.2, 0.0),
+            end: FractionalOffset(1.0, 0.6),
+            stops: [0.0, 0.6],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            _buttonText,
+            style: const TextStyle(
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
