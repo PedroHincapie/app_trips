d6ec0a4d1665742113de85c493046838c2e647ba
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-21 20:06:38 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:app_trips/scr/pages/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'my_app.dart';
+
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
