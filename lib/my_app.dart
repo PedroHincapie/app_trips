85d1b73e300f1ea6202316754ae046df1561d152
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-22 19:42:17 -0500

    Update my_app.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,17 +1,22 @@
+import 'package:app_trips/user/bloc/bloc_user.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 import 'platzi_trips_cupertino.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App Trips',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Lato',
+    return BlocProvider(
+      bloc: BlocUser(),
+      child: MaterialApp(
+        title: 'App Trips',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Lato',
+        ),
+        home: PlatziTripsCupertino(),
       ),
-      home: PlatziTripsCupertino(),
     );
   }
 }
