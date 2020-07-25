49d540df1f33cea5ddbdc667ff5c3671c82a2642
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-25 15:28:30 -0500

    Update button_green.dart

--- lib/widgets/button_green.dart
+++ lib/widgets/button_green.dart
@@ -1,7 +1,19 @@
-import 'package:app_trips/place/ui/widgets/description_place_page.dart';
 import 'package:flutter/material.dart';
 
 class ButtonGreen extends StatefulWidget {
+  final text;
+  double height = 50.0;
+  double width = 50.0;
+  final VoidCallback onPressed;
+
+  ButtonGreen({
+    Key key,
+    @required this.text,
+    @required this.onPressed,
+    this.height,
+    this.width,
+  });
+
   @override
   _ButtonGreenState createState() => _ButtonGreenState();
 }
@@ -10,26 +22,29 @@ class _ButtonGreenState extends State<ButtonGreen> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        Scaffold.of(context).showSnackBar(SnackBar(
-          content: Text("Navegando"),
-        ));
-      },
+      onTap: widget.onPressed,
       child: Container(
-        margin: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
-        height: 50.0,
-        width: 180.0,
+        margin: EdgeInsets.only(
+          top: 30.0,
+          left: 20.0,
+          right: 20.0,
+        ),
+        height: widget.height,
+        width: widget.width,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30.0),
+            borderRadius: BorderRadius.circular(15.0),
             gradient: LinearGradient(
-                colors: [Color(0xFF4268D3), Color(0xFF584CD1)],
+                colors: [
+                  Color(0xFFa7ff84),
+                  Color(0xFF1cbb78),
+                ],
                 begin: FractionalOffset(0.2, 0.0),
                 end: FractionalOffset(1.0, 0.6),
                 stops: [0.0, 0.6],
                 tileMode: TileMode.clamp)),
         child: Center(
           child: Text(
-            buttonText,
+            widget.text,
             style: TextStyle(
                 fontSize: 18.0, fontFamily: "Lato", color: Colors.white),
           ),
