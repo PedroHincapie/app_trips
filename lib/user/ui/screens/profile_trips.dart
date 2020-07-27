bcd148292cce6c44c8ac1546cdaa0bef110d9f8f
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-27 18:25:01 -0500

    Update profile_trips.dart

--- lib/user/ui/screens/profile_trips.dart
+++ lib/user/ui/screens/profile_trips.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/profile_background.dart';
-import 'profile_header.dart';
 import '../widgets/profile_places_list.dart';
+import 'profile_header.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /*return Container(
-      color: Colors.indigo,
-    );*/
     return Stack(
       children: <Widget>[
         ProfileBackground(),
         ListView(
           children: <Widget>[
             ProfileHeader(),
-            ProfilePlacesList()
-
+            ProfilePlacesList(),
           ],
         ),
       ],
     );
   }
-
 }
