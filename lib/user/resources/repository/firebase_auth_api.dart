3c260730043090b714c54bdedabc9c70ca952c20
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-26 20:13:11 -0500

    Create firebase_auth_api.dart

---
+++ lib/user/resources/repository/firebase_auth_api.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthAPI {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<FirebaseUser> sigin() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    FirebaseUser user = await _auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken,
+      ),
+    );
+
+    return user;
+  }
+}
