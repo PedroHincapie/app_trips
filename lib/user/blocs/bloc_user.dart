9e4c448135be0295350b3238afcbf80f513fb757
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-26 20:13:02 -0500

    Update bloc_user.dart

--- lib/user/blocs/bloc_user.dart
+++ lib/user/blocs/bloc_user.dart
@@ -1,6 +1,14 @@
+import 'package:app_trips/user/resources/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 class BlocUser implements Bloc {
+  final _authRepository = AuthRepository();
+
+  //Casos de usuos
+
+  Future<FirebaseUser> signIn() => _authRepository.signInFirebase();
+
   @override
   void dispose() {
     // TODO: implement dispose
