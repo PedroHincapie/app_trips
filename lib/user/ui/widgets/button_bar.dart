d9c68fdf0bcb77db0d55efa7452b605d7e03d739
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-27 16:54:36 -0500

    Update button_bar.dart

--- lib/user/ui/widgets/button_bar.dart
+++ lib/user/ui/widgets/button_bar.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
+
 import 'circle_button.dart';
 
 class ButtonsBar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: EdgeInsets.symmetric(
-            horizontal: 0.0,
-            vertical: 10.0
+          horizontal: 0.0,
+          vertical: 10.0,
         ),
         child: Row(
           children: <Widget>[
-            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.card_travel, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.mail_outline, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(true, Icons.person, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
+            CircleButton(true, Icons.vpn_key, 20.0,
+                Color.fromRGBO(255, 255, 255, 0.6), null),
+            CircleButton(
+              false,
+              Icons.add,
+              40.0,
+              Color.fromRGBO(255, 255, 255, 1),
+              null,
+            ),
+            CircleButton(
+              true,
+              Icons.exit_to_app,
+              20.0,
+              Color.fromRGBO(255, 255, 255, 0.6),
+              _singOut,
+            ),
           ],
-        )
-    );
+        ));
   }
 
+  void _singOut() {
+    print('Pedro salio');
+  }
 }
