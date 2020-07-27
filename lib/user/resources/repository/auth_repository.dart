3366b65df194189644d5bbcf4364cdbb24e0fa90
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-26 20:13:07 -0500

    Create auth_repository.dart

---
+++ lib/user/resources/repository/auth_repository.dart
@@ -0,0 +1,8 @@
+import 'package:app_trips/user/resources/repository/firebase_auth_api.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository {
+  final _firebaseAuth = FirebaseAuthAPI();
+
+  Future<FirebaseUser> signInFirebase() => _firebaseAuth.sigin();
+}
