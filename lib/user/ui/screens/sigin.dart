e0de8a80e6550568d2f631d2c63a4abacac410f1
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-27 16:54:41 -0500

    Update sigin.dart

--- lib/user/ui/screens/sigin.dart
+++ lib/user/ui/screens/sigin.dart
@@ -1,6 +1,8 @@
+import 'package:app_trips/platzi_trips_cupertino.dart';
 import 'package:app_trips/user/blocs/bloc_user.dart';
 import 'package:app_trips/widgets/button_green.dart';
 import 'package:app_trips/widgets/gradient_back.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
@@ -17,7 +19,20 @@ class _SiginState extends State<Sigin> {
   Widget build(BuildContext context) {
     _user = BlocProvider.of(context);
 
-    return _signInGoogleUI();
+    return _handleCurrentSession();
+  }
+
+  Widget _handleCurrentSession() {
+    return StreamBuilder(
+      stream: _user.authStatus,
+      builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+        if (!snapshot.hasData || snapshot.hasError) {
+          return _signInGoogleUI();
+        } else {
+          return PlatziTripsCupertino();
+        }
+      },
+    );
   }
 
   Widget _signInGoogleUI() {
@@ -44,7 +59,7 @@ class _SiginState extends State<Sigin> {
               ButtonGreen(
                 text: 'G Login with Gmail',
                 onPressed: () {
-                  _user.signIn().then((value) => print(value));
+                  _user.signIn().then((value) => print('Este es el valor $value'));
                 },
                 width: 300.0,
                 height: 50.0,
